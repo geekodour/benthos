4b406ffdd5d4978ac9e4b0e18550838a97cd395d
Hrishikesh Barman <oss@geekodour.org>
2024-10-25 02:51:32 +0530

    chore: add redpanda/connect plugins

--- cmd/benthos/main.go
+++ cmd/benthos/main.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/redpanda-data/benthos/v4/public/service"
 
+	// Import full suite of FOSS connect plugins
+	_ "github.com/redpanda-data/connect/public/bundle/free/v4"
+
 	// Import all plugins defined within the repo.
 	_ "github.com/redpanda-data/benthos/v4/public/components/io"
 	_ "github.com/redpanda-data/benthos/v4/public/components/pure"
